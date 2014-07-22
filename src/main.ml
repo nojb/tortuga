2f99f5e83e12daaf34bebdc0c00677a940bc796c
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2014-07-22 11:54:03 +0100

    repl: fix error handling

--- src/main.ml
+++ src/main.ml
@@ -28,25 +28,26 @@ let main () =
   Logo.Control.init env;
   let rec loop () =
     Format.fprintf Format.std_formatter "> @?";
-    let strm = Stream.of_list (Lexer.parse_atoms [] false lexbuf) in
-    Logo.Eval.toplevel env strm;
+    begin
+      try
+        let strm = Stream.of_list (Lexer.parse_atoms [] false lexbuf) in
+        Logo.Eval.toplevel env strm
+      with
+      | Lexer.Error err ->
+        Format.fprintf Format.std_formatter "%a.@." Lexer.report_error err
+      | Logo.Error err ->
+        Format.fprintf Format.std_formatter "%s.@." err
+      | exn ->
+        Format.fprintf Format.std_formatter "internal error: %s@." (Printexc.to_string exn)
+    end;
     loop ()
   in
   try
     loop ()
   with
-  | Lexer.Error err ->
-    Format.fprintf Format.std_formatter "%a.@." Lexer.report_error err;
-    loop ()
-  | Logo.Error err ->
-    Format.fprintf Format.std_formatter "%s.@." err;
-    loop ()
   | Logo.Bye
   | Exit ->
     Format.fprintf Format.std_formatter "Goodbye.@."
-  | exn ->
-    Format.fprintf Format.std_formatter "internal error: %s@." (Printexc.to_string exn);
-    loop ()
  
 let _ =
   print_endline "Welcome to OCaml-Logo";
